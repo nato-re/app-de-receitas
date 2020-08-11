702bca1ac1c289b0dfd12085ffd1439ffee9751b
nato-x <alvares.renato@hotmail.com>
2020-08-10 23:57:07 -0300

    Updates

---
+++ precache-manifest.c589467209e5d0d9bb2ebb4752f60537.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a121dc64839fcd73e1d10a0a9f884b5",
+    "url": "/app-de-receitas/index.html"
+  },
+  {
+    "revision": "4da818d58c435fea7af4",
+    "url": "/app-de-receitas/static/css/main.3329a77a.chunk.css"
+  },
+  {
+    "revision": "ae2af66c995f7ad1c7a1",
+    "url": "/app-de-receitas/static/js/2.24a1fa86.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/app-de-receitas/static/js/2.24a1fa86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4da818d58c435fea7af4",
+    "url": "/app-de-receitas/static/js/main.89b3737e.chunk.js"
+  },
+  {
+    "revision": "b6725d380ca4b62cec70",
+    "url": "/app-de-receitas/static/js/runtime-main.14259213.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/app-de-receitas/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/app-de-receitas/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/app-de-receitas/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/app-de-receitas/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/app-de-receitas/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/app-de-receitas/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/app-de-receitas/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/app-de-receitas/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
